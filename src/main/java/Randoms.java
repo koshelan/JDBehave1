bf9f5bf3fa81511acdfe2dce375b1c5dd8f434d2
ruagegd <elena.kosheleva@raiffeisen.ru>
2021-06-27 17:22:37 +0300

    task 1

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min, max;
+
+    public Randoms(int min, int max) {
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return min + random.nextInt(max-min+1);
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+        return null;
+    }
+}
